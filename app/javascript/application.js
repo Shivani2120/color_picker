87aaecc2227882bd5629b2c77508f7ba27c68aa1
shivani shilpi <shivani.shilpi@clecotech.com>
2022-11-01 19:35:41 +0530

    Added tailwind css

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import '@simonwep/pickr/dist/themes/classic.min.css';
+import '@simonwep/pickr/dist/themes/classic.min.css';import * as bootstrap from "bootstrap"
